354bbc229b359cb8bd657b1ec6de34d40d49b558
RandomGamingDev <ethanshiyi1123@gmail.com>
2023-04-06 14:03:56 -0400

    unflipped

--- image-to-ascii.py
+++ image-to-ascii.py
@@ -8,10 +8,9 @@ img = Image.open(str(input("Enter the name of the image that you would like to c
     int(input("Enter the x resolution of the image: ")),
     int(input("Enter the y resolution of the image: "))
 ), resample=Image.Resampling.BILINEAR)
-img = img.rotate(90, expand = 1)
 
-for x in range(img.size[0]):
-    for y in range(img.size[1]):
+for y in range(img.size[1]):
+    for x in range(img.size[0]):
         pixel = img.getpixel((x, y))
         value = 0
         for color in pixel:
