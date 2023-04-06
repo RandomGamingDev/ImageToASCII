301a16e61754d0de7c204a57b06e29cd2204249c
RandomGamingDev <ethanshiyi1123@gmail.com>
2023-04-06 13:50:32 -0400

    forgot to unlip flip what I flipped to unflip the ASCII image so that's fun lol

--- image-to-ascii.py
+++ image-to-ascii.py
@@ -5,11 +5,10 @@ with open("chars.txt") as file:
     chars = [char.rstrip() for char in file.readlines()]
 
 img = Image.open(str(input("Enter the name of the image that you would like to conver to ASCII: "))).resize((
-    int(input("Enter the y resolution of the image: ")),
-    int(input("Enter the x resolution of the image: "))
+    int(input("Enter the x resolution of the image: ")),
+    int(input("Enter the y resolution of the image: "))
 ), resample=Image.Resampling.BILINEAR)
 img = img.rotate(90, expand = 1)
-img.show()
 
 for x in range(img.size[0]):
     for y in range(img.size[1]):
